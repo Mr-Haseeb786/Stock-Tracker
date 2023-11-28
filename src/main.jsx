b107c42df40f451eaacf58a9dcc54e9a12798462
Muhammad Haseeb <m.haseebofficial786@gmail.com>
2023-11-28 20:19:04 +0500

    more minors

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
-import { Store } from "./Store/store.jsx";
+import { store } from "./Store/store.jsx";
 import { Provider } from "react-redux";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
+  </Provider>
 );
